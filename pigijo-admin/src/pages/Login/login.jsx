c7919cbc3050709c12f988ce3e362350c06ba542
Muhammad Hafidz Nurrohman <hafidzn04@gmail.com>
2024-01-09 14:43:32 +0700

    update react

--- pigijo-admin/src/pages/Login/login.jsx
+++ pigijo-admin/src/pages/Login/login.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { React, useEffect } from "react";
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
