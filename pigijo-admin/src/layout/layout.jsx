5456d7b0fa7aefb71de2a8a0a19d59125c9b3fc1
Muhammad Hafidz Nurrohman <hafidzn04@gmail.com>
2024-01-09 14:46:06 +0700

    Update layout.jsx

--- pigijo-admin/src/layout/layout.jsx
+++ pigijo-admin/src/layout/layout.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { React, useState, useEffect } from "react";
 import Navbar from "../component/Navbar/navbar";
 import Routers from "./routers";
 
